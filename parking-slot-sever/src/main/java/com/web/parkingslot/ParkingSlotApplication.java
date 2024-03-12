b5155af5990adc02761c40f93ae1892ce75f5319
Draco <102514108+Drac0Nguyen@users.noreply.github.com>
2024-03-12 22:18:19 +0700

    Initial commit

---
+++ parking-slot-sever/src/main/java/com/web/parkingslot/ParkingSlotApplication.java
@@ -0,0 +1,13 @@
+package com.web.parkingslot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSlotApplication.class, args);
+	}
+
+}
